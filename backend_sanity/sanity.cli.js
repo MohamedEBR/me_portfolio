a67a489fd3d1f395ad981db1c5e4434ebdde7af4
MohamedEBR <110148669+MohamedEBR@users.noreply.github.com>
2024-02-15 13:23:31 -0500

    Clean up ignored files

--- backend_sanity/sanity.cli.js
+++
@@ -1,9 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-require("dotenv").config()
-
-export default defineCliConfig({
-  api: {
-    projectId: process.env.ID,
-    dataset: process.env.DATASET
-  }
-})
