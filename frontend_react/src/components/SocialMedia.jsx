e6b87b04cca0aaabd5909721ff60b9a2c9dd767a
MohamedEBR <110148669+MohamedEBR@users.noreply.github.com>
2024-03-03 22:16:19 -0500

    Update SocialMedia.jsx

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { BsTwitter, BsInstagram} from 'react-icons/bs'
-import { FaFacebookF} from 'react-icons/fa'
+import React from "react";
+import { BsLinkedin, BsInstagram } from "react-icons/bs";
 
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-        <div>
-            <BsTwitter />    
-        </div>
-        <div>
-            <FaFacebookF />
-        </div>
-        <div>
-            <BsInstagram />
-        </div>
+    <div className="app__social">
+      <div>
+        <a href="https://www.linkedin.com/in/mohamed-ebraheem-294541240/">
+          <BsLinkedin />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.instagram.com/momo.k.champ/">
+          <BsInstagram />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
